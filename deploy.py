2d8efbeeff072623160d4c7efeb956d1d2bd8d34
xavi-b <xavi-b@users.noreply.github.com>
2019-03-24 21:41:56 +0100

    Copie des fichiers changée

--- deploy.py
+++ deploy.py
@@ -5,7 +5,8 @@ import os;
 
 with open(".gitignore","r") as file:
     for line in file:
+        line = line.strip('/');
         if line.startswith("!") and not os.path.basename(__file__) in line:
             print("Copying " + line[1:-1]);
-            subprocess.call("cp -r " + line[1:-1] + " ~/", shell=True);
+            subprocess.call("cp -r " + line[1:-1] + " ~/" + line[1:-1], shell=True);
 print("Done");
