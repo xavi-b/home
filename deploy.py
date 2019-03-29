4eb1cae5f899a7aaeb1486b8a444bc3b9b7cce8a
xavi-b <xavi-b@users.noreply.github.com>
2019-03-29 18:48:47 +0100

    Correction gitignore + meilleure gestion du deploy

--- deploy.py
+++ deploy.py
@@ -5,7 +5,8 @@ import os;
 
 with open(".gitignore","r") as file:
     for line in file:
-        line = line.strip().strip("/");
-        if line.startswith("!") and not os.path.basename(__file__) in line:
+        line = line.strip();
+        if line.startswith("!") and not line.endswith("*") and not os.path.basename(__file__) in line:
+            line = line.strip("/");
             print("Copying " + line[1:] + " : " + str(subprocess.call("cp -rT " + line[1:] + " ~/" + line[1:], shell=True)));
 print("Done");
