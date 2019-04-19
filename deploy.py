bbe931081de839937037b2312755fc1bf5da7338
xavi-b <developer@xavi-b.fr>
2019-04-19 20:11:00 +0200

    Suppression de la copie du gitignore dans le home

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ import os;
 with open(".gitignore","r") as file:
     for line in file:
         line = line.strip();
-        if line.startswith("!") and not line.endswith("*") and not os.path.basename(__file__) in line:
+        if line.startswith("!") and not line.endswith("*") and not ".gitignore" in line and not os.path.basename(__file__) in line:
             line = line.strip("/");
             print("Copying " + line[1:] + " : " + str(subprocess.call("cp -rT " + line[1:] + " ~/" + line[1:], shell=True)));
 print("Done");
