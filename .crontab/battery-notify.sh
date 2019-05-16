b01101ff4390dbb064ee05679d74ea9941454d63
xavi-b <developer@xavi-b.fr>
2019-05-16 23:33:10 +0200

    Corrections in order to notify-send to work

--- .crontab/battery-notify.sh
+++ .crontab/battery-notify.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+export DISPLAY=:0.0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
 battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 
 if [ $battery_level -le 30 ]; then
-    notify-send -a "Battery notify" "Battery low" "${battery_level}%"
+    /usr/bin/notify-send -a "Battery notify" "Battery low" "${battery_level}%"
 elif [ $battery_level -le 10 ]; then
-    notify-send -a "Battery notify" "Battery low" "${battery_level}%" -u critical
+    /usr/bin/notify-send -a "Battery notify" "Battery low" "${battery_level}%" -u critical
 fi
