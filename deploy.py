8b3980f0f8292d9b00dc5cbbef92b316daf1a62e
xavi-b <developer@xavi-b.fr>
2020-09-01 18:48:43 +0200

    Copy hostcolor.sh only when it does not exists already

--- deploy.py
+++ deploy.py
@@ -2,6 +2,7 @@
 
 import subprocess;
 import os;
+import shutil;
 
 with open(".gitignore","r") as file:
     for line in file:
@@ -9,4 +10,6 @@ with open(".gitignore","r") as file:
         if line.startswith("!") and not line.endswith("*") and not ".gitignore" in line and not os.path.basename(__file__) in line:
             line = line.strip("/");
             print("Copying " + line[1:] + " : " + str(subprocess.call("cp -rT " + line[1:] + " ~/" + line[1:], shell=True)));
+if not os.path.exists(os.path.expanduser("~/.bash/hostcolor.sh")):
+    shutil.copy("hostcolor.sh", os.path.expanduser("~/.bash"));
 print("Done");
